7937814fac6f62b5dfb9c152407e57008eeedf1f
Keifiek <c.ivanovich.214@gmail.com>
2025-04-11 09:23:11 -0600

    Clase 11Abril: Código terminado.

--- SPA/11abril/grafo.py
+++ SPA/11abril/grafo.py
@@ -6,5 +6,23 @@ class Grafo:
 		self.grafo: Dict[Punto, Set[Punto]] = {punto: set() for punto in puntos}
 
 	def agregar_conexion(self, punto1: Punto, punto2: Punto):
+		uno2 = 2
+		if (not isinstance(punto1, Punto) and (uno2 := 1)) or (not isinstance(punto2, Punto)):
+			raise ValueError(f"El punto {uno2} no es válido.")
+		if punto1 == punto2:
+			raise ValueError("ERROR: Se esta tratanto de conectar un punto a si mismo. Los puntos deben ser diferentes.")
 		self.grafo[punto1].add(punto2)
 		self.grafo[punto2].add(punto1)
+
+	def eliminar_conexion(self, punto1: Punto, punto2: Punto):
+		malPunto = 0
+		if not isinstance(punto1, Punto): 
+			malPunto = 1
+		if not isinstance(punto2, Punto):
+			raise ValueError(f"El punto {malPunto} no es válido.")
+		if punto1 == punto2:
+			raise ValueError("ERROR: Se esta tratanto de conectar un punto a si mismo. Los puntos deben ser diferentes.")
+		if punto1 not in self.grafo[punto2]:
+			raise ValueError("No hay conexión entre los puntos.")
+		self.grafo[punto1].remove(punto2)
+		self.grafo[punto2].remove(punto1)
--- SPA/11abril/punto.py
+++ SPA/11abril/punto.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from random import randint, seed
 
 @dataclass
 class Punto:
@@ -14,3 +15,7 @@ class Punto:
     
     def __hash__(self):
         return hash((self.x, self.y))
+
+seed(42)
+def generar_punto() -> Punto:
+    return Punto(x=randint(0, 500), y=randint(0, 500), red=randint(0, 255), blue=randint(0, 255), green=randint(0, 255), radio=randint(0, 50))
